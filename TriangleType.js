60198710eddc76f5b711820b41d93af63fc0dd44
Peter Halligan <peter.halligan@ie.ibm.com>
2018-11-29 02:56:30 +0000

    tests for number of arguments and compares equality of sides

--- TriangleType.js
+++ TriangleType.js
@@ -1,6 +1,14 @@
 const TriangleType = (a, b, c) => {
-  if (a === b && b === c) return "equilateral";
-  if (a !==b && b !== c && a !==c) return "scalene"
+  //todo: check for valid types
+  return new Promise((resolve, reject) =>{
+    if (!a || !b || !c) return reject("not_enough_arguments");
+    if (a === b && b === c) return resolve("equilateral");
+    if (a !== b && b !== c && a !==c) return resolve("scalene");
+    //could leave this as an else
+    if ((a === b && b !== c) || (a === c && a !== b) || a !== b && b === c) return resolve("isosceles");
+
+  });
+
 
 };
 
--- TriangleType.spec.js
+++ TriangleType.spec.js
@@ -1,14 +1,76 @@
 const TriangleType = require("./TriangleType");
 
-test("should be defined", () =>{
+test("should be defined", () => {
   expect(TriangleType).toBeDefined();
 });
 
-test("should identify an equilateral triangle if all sides are even", () => {
-  expect(TriangleType(1, 1, 1)).toBe("equilateral");
+test("should identify an equilateral triangle if all sides are even", async () => {
+  expect.assertions(1);
+  const response = await TriangleType(1, 1, 1);
+  expect(response).toBe("equilateral");
+
 });
 
-test("should identify an scalene triangle if all sides are not equal", () => {
-  expect(TriangleType(1, 2, 3)).toBe("scalene");
+test("should identify an scalene triangle if all sides are not equal", async () => {
+  expect.assertions(1);
+  const response = await TriangleType(1, 2, 3);
+  expect(response).toBe("scalene");
 });
 
+test("should identify an isosceles triangle if 2 sides are equal", async () => {
+  expect.assertions(1);
+  const response = await TriangleType(1, 1, 3);
+  expect(response).toBe("isosceles");
+});
+
+test("should identify an isosceles triangle if 2 sides are equal", async () => {
+  expect.assertions(1);
+  const response = await TriangleType(1, 3, 1);
+  expect(response).toBe("isosceles");
+
+});
+
+test("should identify an isosceles triangle if 2 sides are equal", async () => {
+  const response = await TriangleType(3, 1, 1);
+  expect(response).toBe("isosceles");
+});
+
+test("should return an error if not provided with 3 args", async () => {
+  expect.assertions(1);
+  try {
+    await TriangleType(3, 1);
+  } catch (e) {
+    expect(e).toMatch("not_enough_arguments");
+  }
+
+});
+
+test("should return an error if an arg is undefined", async () => {
+  expect.assertions(1);
+  try {
+    await TriangleType(3, 1, undefined);
+  } catch (e) {
+    expect(e).toMatch("not_enough_arguments");
+  }
+
+});
+
+test("should return an error if an arg is null", async () => {
+  expect.assertions(1);
+  try {
+    await TriangleType(null, 1, 1);
+  } catch (e) {
+    expect(e).toMatch("not_enough_arguments");
+  }
+
+});
+
+test("should return an error if a side is 0", async () => {
+  expect.assertions(1);
+  try {
+    await TriangleType(0, 1, 1);
+  } catch (e) {
+    expect(e).toMatch("not_enough_arguments");
+  }
+
+});
